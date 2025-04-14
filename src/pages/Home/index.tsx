39c47a6518e9ac4760bfc6a4090c3d38e549356f
Laci Szalai <laci.szalai@regnology.net>
2025-04-14 08:27:17 +0200

    I'm in 32/. Git pull gave me 2 biome-related commits, where from? CUrrent commit is what I found in 32/ staying here. 3 fires is extra.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,78 +1,78 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import CardList from 'components/CardList';
-import { CardFromApi, CardType } from 'types/index';
+import axios from "axios";
+import CardList from "components/CardList";
+import { useEffect, useState } from "react";
+import type { CardFromApi, CardType } from "types/index";
 
 function RateCards() {
-  const [cardList, setCardList] = useState<CardType[]>([]);
+	const [cardList, setCardList] = useState<CardType[]>([]);
 
-  useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_SERVERIP}/cards`)
-      .then((response) => {
-        setCardList(
-          response.data.map((card: CardFromApi) => ({
-            ...card,
-            rate: card.rate ? parseInt(card.rate) : card.rate,
-            sides: JSON.parse(card.sides),
-          })),
-        );
-      })
-      .catch((error) => {
-        console.error('Error fetching flashcards:', error);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get(`${import.meta.env.VITE_SERVERIP}/cards`)
+			.then((response) => {
+				setCardList(
+					response.data.map((card: CardFromApi) => ({
+						...card,
+						rate: card.rate ? parseInt(card.rate) : card.rate,
+						sides: JSON.parse(card.sides),
+					})),
+				);
+			})
+			.catch((error) => {
+				console.error("Error fetching flashcards:", error);
+			});
+	}, []);
 
-  const handleRateCard = (card: CardType, rate: number) => {
-    const updatedCardList = cardList.map((cardItem) => {
-      if (card.id === cardItem.id) {
-        return { ...cardItem, rate, reviewedAt: Date.now() };
-      }
-      return cardItem;
-    });
+	const handleRateCard = (card: CardType, rate: number) => {
+		const updatedCardList = cardList.map((cardItem) => {
+			if (card.id === cardItem.id) {
+				return { ...cardItem, rate, reviewedAt: Date.now() };
+			}
+			return cardItem;
+		});
 
-    axios
-      .put(`${import.meta.env.VITE_SERVERIP}/cards/${card.id}`, {
-        ...card,
-        sides: JSON.stringify(card.sides),
-        rate,
-        reviewedAt: Date.now(),
-      })
-      .then(() => {
-        setCardList(updatedCardList);
-      })
-      .catch((error) => {
-        console.error('Error updating flashcard:', error);
-      });
-  };
+		axios
+			.put(`${import.meta.env.VITE_SERVERIP}/cards/${card.id}`, {
+				...card,
+				sides: JSON.stringify(card.sides),
+				rate,
+				reviewedAt: Date.now(),
+			})
+			.then(() => {
+				setCardList(updatedCardList);
+			})
+			.catch((error) => {
+				console.error("Error updating flashcard:", error);
+			});
+	};
 
-  return (
-    <div className="app-container">
-      <div className="background">
-        <div className="background-base" />
-        <div className="background-middle">
-          <div className="diagonal-section-middle" />
-        </div>
-        <div className="background-top">
-          <div className="diagonal-section-top" />
-        </div>
-      </div>
+	return (
+		<div className="app-container">
+			<div className="background">
+				<div className="background-base" />
+				<div className="background-middle">
+					<div className="diagonal-section-middle" />
+				</div>
+				<div className="background-top">
+					<div className="diagonal-section-top" />
+				</div>
+			</div>
 
-      <div className="content">
-        <div className="header">
-          <h1>SmartAnki Pro</h1>
-          <div className="streak">
-            <span>Daily Streak: 42</span>
-            <span>🔥</span>
-          </div>
-          <div className="progress-bar">
-            <div className="progress-fill"></div>
-          </div>
-        </div>
-        <CardList cards={cardList} onRateCard={handleRateCard} />
-      </div>
-    </div>
-  );
+			<div className="content">
+				<div className="header">
+					<h1>SmartAnki Pro</h1>
+					<div className="streak">
+						<span>Daily Streak: 42</span>
+						<span>🔥🔥🔥</span>
+					</div>
+					<div className="progress-bar">
+						<div className="progress-fill" />
+					</div>
+				</div>
+				<CardList cards={cardList} onRateCard={handleRateCard} />
+			</div>
+		</div>
+	);
 }
 
 export default RateCards;
